688b7fb81c081ffeaafa271d334aa5316b14e0a0
Steve <stevewu24@gmail.com>
2024-07-16 17:50:41 -0700

    created and updated the following models updated application record model updated event attendee model updated event model updated user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,14 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User"
-  has_many :attendees, through: :event_attendees, source: :event_attendee # return users that are attending
-  has_many :event_attendees, foreign_key: :attendee_id
+  belongs_to :creator, foreign_key: "creator_id", class_name: "User"
+  has_many :event_attendees, foreign_key: :attended_events_id
+  has_many :attendees, through: :event_attendees, source: :attendee # return users that are attending
 
+  validates :title, length: { minimum: 4 }, presence: true
+  validates :description, length: { minimum: 4 }, presence: true
+  validates :location, presence: true
+  validates :start_date, presence: { message: "Date must be given" }
+  validates :start_time, presence: { message: "Time must be given" }
+
+  scope :past_events, -> { where("DATE(start_date) <= ?", Date.today) }
+  scope :upcoming_events, -> { where("DATE(start_date) >= ?", Date.today)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many :created_events, foreign_key: "creator_id", class_name: 'Event' # returns created events
-  has_many :attended_events, through: :event_attendees # return events that were attended
-  has_many :event_attendees, foreign_key: :event_id # return all events that belong to the user
+  
+  # event_attendees must load first before using the through: :event_attendees
+  has_many :event_attendees, foreign_key: :attendee_id # return events the user attended
+  has_many :all_events, through: :event_attendees, source: :attended_events # return events that were attended
+  has_many :created_events, foreign_key: "creator_id", class_name: "Event" # returns created events
+
+  validates :username, length: { minimum: 4 }
 end
